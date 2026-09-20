d2d6741dddd561b0c287009430853ff4307962e3
ylosifer <ylosifer001@gmail.com>
2026-09-20 21:31:37 +0530

    Configure frontend production API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
   headers: {
