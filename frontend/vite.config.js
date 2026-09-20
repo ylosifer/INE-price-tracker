6c42619592cdf2926fc7e484c0953200a512f8bd
ylosifer <ylosifer001@gmail.com>
2026-09-20 21:38:37 +0530

    Allow Render preview host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['ine-price-tracker-1-jalj.onrender.com'],
+  },
 })
